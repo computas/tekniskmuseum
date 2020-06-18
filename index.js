4e3e2f2b7e0040e71571b8078a27d528eff937ef
Trond Albinussen <tal@computas.com>
2020-06-18 11:28:08 +0200

    added node test

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello Node!56\n'
+  res.end(msg);
+});
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
+});
