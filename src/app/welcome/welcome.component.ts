4bda19a2e0f1be1f1503acedae763d7a4f000cc4
Bjørn Harbo Dahle <bjdax@computas.com>
2020-06-26 15:07:00 +0200

    feat(welcomePage): added start game button

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+// import { AppModule } from "../app.module";
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  startGame() {
+    this.router.navigateByUrl('/drawing');
   }
-
 }
