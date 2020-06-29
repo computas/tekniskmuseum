c8a7cc58d98408c0c379040c56d738cbede47fe2
Peder Aalen <pewax@computas.com>
2020-06-29 10:34:32 +0200

    fix: test fail - import HttpClientTestingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WelcomeModule } from './welcome/welcome.module';
 import { DrawingModule } from './drawing/drawing.module';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     WelcomeModule,
     DrawingModule,
-    HttpClientModule,
   ],
-  providers: [],
+  providers: [HttpClientModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/drawing/services/drawing.service.ts
+++ src/app/drawing/services/drawing.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root',
 })
