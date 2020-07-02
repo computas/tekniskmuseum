ec0abd54a778490e24c8b70f5cc4aa7ff1cb0dce
Ole Martin Larsen <ollax@computas.com>
2020-07-02 09:24:58 +0200

    feat(result): added result screen which uses the result behaviour subject to inform the user if the answer was correct and a button for new drawing

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { DrawingService } from '../drawing/services/drawing.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
-  styleUrls: ['./result.component.scss']
+  styleUrls: ['./result.component.scss'],
 })
 export class ResultComponent implements OnInit {
+  result: boolean;
 
-  constructor() { }
+  constructor(private drawingService: DrawingService, private router: Router) {}
 
   ngOnInit(): void {
+    this.getResult();
   }
 
+  getResult() {
+    this.drawingService.resultSource.subscribe(
+      (result) => ((this.result = result), console.log(result))
+    );
+  }
+
+  newDrawing() {
+    this.router.navigate(['/drawing']);
+  }
 }
