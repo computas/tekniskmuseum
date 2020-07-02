b54c3173175731a1cae377a0c127b28bdb574a50
Ole Martin Larsen <ollax@computas.com>
2020-07-02 11:29:18 +0200

    feat(result): imported result module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeModule } from './welcome/welcome.module';
 import { DrawingModule } from './drawing/drawing.module';
 import { GameInfoModule } from './game-info/game-info.module';
+import { ResultModule } from './result/result.module';
 import { ResultComponent } from './result/result.component';
 
-
 @NgModule({
-  declarations: [AppComponent, ResultComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
@@ -21,6 +22,7 @@ import { ResultComponent } from './result/result.component';
     WelcomeModule,
     DrawingModule,
     GameInfoModule,
+    ResultModule,
   ],
   providers: [HttpClientModule],
   bootstrap: [AppComponent],
