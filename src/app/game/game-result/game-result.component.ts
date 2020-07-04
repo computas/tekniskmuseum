e156bdbd1b2de5e1cbd1825390e518cc2d8efdbe
Peder Aalen <pewax@computas.com>
2020-07-04 19:48:41 +0200

    add: game-result component

---
+++ src/app/game/game-result/game-result.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-result',
+  templateUrl: './game-result.component.html',
+  styleUrls: ['./game-result.component.scss']
+})
+export class GameResultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -5,8 +5,9 @@ import { GameDrawComponent } from './game-draw/game-draw.component';
 import { GameComponent } from './game.component';
 import { MaterialImportsModule } from '../shared/material-imports/material-imports.module';
 import { GameIntermediateResultComponent } from './game-intermediate-result/game-intermediate-result.component';
+import { GameResultComponent } from './game-result/game-result.component';
 @NgModule({
-  declarations: [GameInfoComponent, GameDrawComponent, GameComponent, GameIntermediateResultComponent],
+  declarations: [GameInfoComponent, GameDrawComponent, GameComponent, GameIntermediateResultComponent, GameResultComponent],
   imports: [CommonModule, MaterialImportsModule],
 })
 export class GameModule {}
