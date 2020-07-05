12d6754bca084a873d7e53d9d94d375b20353844
Peder Aalen <pewax@computas.com>
2020-07-05 23:03:23 +0200

    fix: rebase changes and check that everything is ok

--- src/app/game/game-step/game-step.component.spec.ts
+++ src/app/game/game-step/game-step.component.spec.ts
@@ -8,9 +8,8 @@ describe('GameStepComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GameStepComponent ]
-    })
-    .compileComponents();
+      declarations: [GameStepComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
--- src/app/game/game-step/game-step.component.ts
+++ src/app/game/game-step/game-step.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-game-step',
   templateUrl: './game-step.component.html',
-  styleUrls: ['./game-step.component.scss']
+  styleUrls: ['./game-step.component.scss'],
 })
 export class GameStepComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
