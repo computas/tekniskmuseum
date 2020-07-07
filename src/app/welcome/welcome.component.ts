38a826f5d8c4ef2530977a81d4497977cce88d70
Ole Martin Larsen <ollax@computas.com>
2020-07-07 09:10:23 +0200

    feat(mediaqueries): add mobile query that listens for changes in resolution

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { routes } from '../shared/models/routes';
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss'],
 })
-export class WelcomeComponent implements OnInit {
-  constructor(private router: Router) {}
+export class WelcomeComponent implements OnDestroy {
+  mobileQuery: MediaQueryList;
 
-  ngOnInit(): void {}
+  private _mobileQueryListener: () => void;
+
+  constructor(private router: Router, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => {
+      changeDetectorRef.detectChanges();
+    };
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 
   goToGameInfoPage() {
     this.router.navigate([routes.PLAYGAME]);
