72c2ec474536a99dc98b8c435d7fe3e0314643fb
Ole Martin Larsen <ollax@computas.com>
2020-07-12 01:58:01 +0200

    feat(design): add tts to word to draw page

--- src/app/shared/speech-text/text.ts
+++ src/app/shared/speech-text/text.ts
@@ -1,4 +1,5 @@
 export const SPEECH = {
   welcome: 'Hei, jeg er Kunstig Intelligens. Kan du hjelpe meg lære?',
   info: 'Slik spiller du: 1. Tegn ord. 2. Jeg gjetter. 3. Få poeng.',
+  draw: 'Tegn: ',
 };
