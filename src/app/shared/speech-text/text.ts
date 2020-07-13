28b8eabfe14993ad32dfce7245f1b65b98de721d
Ole Martin Larsen <ollax@computas.com>
2020-07-13 08:55:53 +0200

    feat(design): add intermediate result speech

--- src/app/shared/speech-text/text.ts
+++ src/app/shared/speech-text/text.ts
@@ -2,4 +2,6 @@ export const SPEECH = {
   welcome: 'Hei, jeg er Kunstig Intelligens. Kan du hjelpe meg lære?',
   info: 'Slik spiller du: 1. Tegn ord. 2. Jeg gjetter. 3. Få poeng.',
   draw: 'Tegn: ',
+  resultWon: 'Hurra, jeg har lært meg ',
+  resultLoss: 'Beklager, jeg klarte ikke gjette',
 };
