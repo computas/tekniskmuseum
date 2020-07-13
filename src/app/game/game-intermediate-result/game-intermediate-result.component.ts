c047bbc38bf0f00d545c49513241996ffb13209f
Ole Martin Larsen <ollax@computas.com>
2020-07-13 08:57:31 +0200

    feat(design): add speakresult method for tts on intermediate result

--- src/app/game/game-intermediate-result/game-intermediate-result.component.ts
+++ src/app/game/game-intermediate-result/game-intermediate-result.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Result } from '../../shared/models/result.interface';
 import { DrawingService } from '../game-draw/services/drawing.service';
+import { SPEECH } from 'src/app/shared/speech-text/text';
+import { SpeechService } from 'src/app/services/speech.service';
 @Component({
   selector: 'app-game-intermediate-result',
   templateUrl: './game-intermediate-result.component.html',
@@ -8,9 +10,11 @@ import { DrawingService } from '../game-draw/services/drawing.service';
 })
 export class GameIntermediateResultComponent implements OnInit {
   result: Result;
+  wonSentence = SPEECH.resultWon;
+  lostSentence = SPEECH.resultLoss;
   @Output() nextGuess = new EventEmitter();
 
-  constructor(private drawingService: DrawingService) {}
+  constructor(private drawingService: DrawingService, private speechService: SpeechService) {}
 
   ngOnInit(): void {
     this.result = this.drawingService.lastResult;
@@ -18,5 +22,13 @@ export class GameIntermediateResultComponent implements OnInit {
 
   newDrawing() {
     this.nextGuess.next(true);
+  }
+
+  speakResult() {
+    if (this.result.hasWon) {
+      this.speechService.speak(`${SPEECH.resultWon}${this.result.word}`);
+    } else {
+      this.speechService.speak(SPEECH.resultLoss);
+    }
   }
 }
