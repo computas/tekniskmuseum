97d111827ae05b3554759e840f7116b995a81307
Ole Martin Larsen <ollax@computas.com>
2020-07-14 12:46:30 +0200

    fix: update wording on welcome page

--- src/app/shared/speech-text/text.ts
+++ src/app/shared/speech-text/text.ts
@@ -1,7 +1,7 @@
 export const SPEECH = {
   correctGuess: 'Jeg vet hva det er, det er ',
   couldntGuess: 'Jeg greide dessverre ikke gjette hva du tegnet.',
-  welcome: 'Hei, jeg er Kunstig Intelligens. Kan du hjelpe meg lære?',
+  welcome: 'Hei, jeg heter Kunstig Intelligens. Kan du hjelpe meg å lære?',
   info: 'Slik spiller du: 1. Tegn ord. 2. Jeg gjetter. 3. Få poeng.',
   draw: 'Tegn: ',
   resultWon: 'Hurra, jeg har lært meg ',
