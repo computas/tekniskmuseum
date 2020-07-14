adb9b665e80f67c1021d596464f122a9b15e13be
Ole Martin Larsen <ollax@computas.com>
2020-07-14 15:24:44 +0200

    fix: add back button and avatar

--- src/app/game/game-info/game-info.component.ts
+++ src/app/game/game-info/game-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { routes } from '../../shared/models/routes';
 import { SPEECH } from '../../shared/speech-text/text';
 import { SpeechService } from 'src/app/services/speech.service';
 
@@ -10,12 +12,16 @@ import { SpeechService } from 'src/app/services/speech.service';
 export class GameInfoComponent implements OnInit {
   @Output() getDrawWord = new EventEmitter();
 
-  constructor(private speechService: SpeechService) {}
+  constructor(private speechService: SpeechService, private router: Router) {}
 
   ngOnInit(): void {}
 
   startDrawing() {
     this.getDrawWord.emit(true);
+  }
+
+  goToLanding() {
+    this.router.navigate([routes.LANDING]);
   }
 
   speakInfo() {
