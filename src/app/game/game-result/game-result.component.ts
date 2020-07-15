1c29f0fd32633a8819b78bd9df2e249693a20f33
Peder Aalen <pewax@computas.com>
2020-07-15 12:13:50 +0200

    add mock on summary route and remove autofocus

--- src/app/game/game-result/game-result.component.ts
+++ src/app/game/game-result/game-result.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DrawingService } from '../game-draw/services/drawing.service';
 import { Result } from '../../shared/models/result.interface';
 import { HighScoreService } from 'src/app/services/highscore.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-game-result',
@@ -10,10 +11,18 @@ import { HighScoreService } from 'src/app/services/highscore.service';
 })
 export class GameResultComponent implements OnInit {
   results: Result[] = [];
-  constructor(private drawingService: DrawingService, private highScoreService: HighScoreService) {}
+  constructor(
+    private drawingService: DrawingService,
+    private highScoreService: HighScoreService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.highScoreService.getAllHighScores().subscribe((res) => {});
-    this.results = this.drawingService.results;
+    if (this.router.url === '/summary') {
+      this.results = this.drawingService.get();
+    } else {
+      this.results = this.drawingService.results;
+    }
   }
 }
