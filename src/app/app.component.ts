bb5637842f0c6fb3b85849b3d3066127c11c4f97
Bjørn <bjdax@computas.com>
2020-07-16 10:28:51 +0200

    generated component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, HostListener } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Router } from '@angular/router';
+import {MatDialogModule} from '@angular/material/dialog';
+
 
 
 @Component({
@@ -12,21 +14,39 @@ export class AppComponent {
   title = 'Teknisk Museum';
 
   userActivity;
-
+  // userActivity2;
   userInactive: Subject<any> = new Subject();
+  // userInactive2: Subject<any> = new Subject();
+
   constructor(private router: Router) {
-
+    console.log(this.router.url);
     this.setTimeout();
-    this.userInactive.subscribe(() => this.router.navigateByUrl(''));
 
+    // this.userInactive.subscribe(() => alert("Spillet starter på nytt om 10 sekunder."));
+
+    this.userInactive.subscribe(() => {
+      if (this.router.url != '/') {
+        // alert("timeout");
+        // this.router.navigateByUrl('');
+        console.log(this.router.url, "inaktiv");
+        // this.userInactive2.subscribe(() => console.log(this.router.url, "starter igjen"));
+        setTimeout(() => console.log(this.router.url, "starter igjen"), 1500);
+
+      }
+    });
   }
 
   setTimeout() {
     this.userActivity = setTimeout(() => this.userInactive.next(undefined), 3000);
+    // this.userActivity2 = setTimeout(() => this.userInactive2.next(undefined), 1500);
   }
 
-  @HostListener('window:mousemove') refreshUserState() {
+  //legge til dersom bilde endrer seg? ?
+  @HostListener('window:mousemove')
+  @HostListener('document:touchmove')
+  refreshUserState() {
     clearTimeout(this.userActivity);
+    // clearTimeout(this.userActivity2);
     this.setTimeout();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { WelcomeModule } from './welcome/welcome.module';
 import { GameModule } from './game/game.module';
 import { HighScoreModule } from './highscore/highscore.module';
+import { IdleDialogComponent } from './idle-dialog/idle-dialog.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, IdleDialogComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule, WelcomeModule, GameModule, HighScoreModule],
   providers: [HttpClientModule],
   bootstrap: [AppComponent],
