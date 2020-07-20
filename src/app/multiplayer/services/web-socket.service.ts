ee20855c8838f104b887f52c70a598882f186703
Peder Aalen <pewax@computas.com>
2020-07-20 12:46:31 +0200

    add: websocket service implementation

--- src/app/multiplayer/services/web-socket.service.ts
+++ src/app/multiplayer/services/web-socket.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+import { tap, catchError } from 'rxjs/operators';
+import { Subject, EMPTY } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WebSocketService {
+  WS_ENDPOINT = 'ws://localhost:8000';
+  private socket$: WebSocketSubject<any>;
 
-  constructor() { }
+  private messagesSubject$ = new Subject();
+  constructor() {}
+
+  public connect(): void {
+    if (!this.socket$ || this.socket$.closed) {
+      this.socket$ = this.getNewWebSocket();
+      const messages = this.socket$.pipe(
+        tap({
+          error: (error) => console.error(error),
+        }),
+        catchError((_) => EMPTY)
+      );
+      this.messagesSubject$.next(messages);
+    }
+  }
+
+  private getNewWebSocket() {
+    return webSocket(this.WS_ENDPOINT);
+  }
+
+  sendMessage(msg: any) {
+    this.socket$.next(msg);
+  }
+
+  close() {
+    this.socket$.complete();
+  }
 }
