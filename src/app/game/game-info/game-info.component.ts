dd4b3cf2d04d2aeab84cf86ee003979342804781
Peder Aalen <pewax@computas.com>
2020-07-20 13:16:12 +0200

    add: single and multiplayer info header

--- src/app/game/game-info/game-info.component.ts
+++ src/app/game/game-info/game-info.component.ts
@@ -12,12 +12,15 @@ import { SpeechService } from 'src/app/services/speech.service';
 export class GameInfoComponent implements OnInit {
   @Output() getDrawWord = new EventEmitter();
   isSinglePlayer = false;
+  isMultiPlayer = false;
 
   constructor(private speechService: SpeechService, private router: Router) {}
 
   ngOnInit(): void {
-    if (this.router.url === routes.SINGLEPLAYER) {
+    if (this.router.url === `/${routes.SINGLEPLAYER}`) {
       this.isSinglePlayer = true;
+    } else {
+      this.isMultiPlayer = true;
     }
   }
 
