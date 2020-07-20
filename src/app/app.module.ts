6c311ae3a7c82bcaa010714f549d9221104ab5f2
Ole Martin Larsen <ollax@computas.com>
2020-07-20 10:22:47 +0200

    feature/multiplayer: add gamemode module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,22 @@ import { AppComponent } from './app.component';
 import { WelcomeModule } from './welcome/welcome.module';
 import { GameModule } from './game/game.module';
 import { HighScoreModule } from './highscore/highscore.module';
+import { GameModeModule } from './game-mode/game-mode.module';
 import { MultiplayerComponent } from './multiplayer/multiplayer.component';
 import { LobbyComponent } from './multiplayer/lobby/lobby.component';
 import { GameModeComponent } from './game-mode/game-mode.component';
 
 @NgModule({
   declarations: [AppComponent, MultiplayerComponent, LobbyComponent, GameModeComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, WelcomeModule, GameModule, HighScoreModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    WelcomeModule,
+    GameModule,
+    HighScoreModule,
+    GameModeModule,
+  ],
   providers: [HttpClientModule],
   bootstrap: [AppComponent],
 })
