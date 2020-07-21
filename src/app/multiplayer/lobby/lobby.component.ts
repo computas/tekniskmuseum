4eaf0901b069ee908f002d3276340833ad458a12
Ole Martin Larsen <ollax@computas.com>
2020-07-21 11:20:41 +0200

    feat(multiplayer): add listen to state and game info

--- src/app/multiplayer/lobby/lobby.component.ts
+++ src/app/multiplayer/lobby/lobby.component.ts
@@ -1,7 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { MultiplayerService } from '../services/multiplayer.service';
 import { WebSocketService } from '../services/web-socket.service';
-import { GameInfo } from '../services/game-info';
+import { routes } from '../../shared/models/routes';
+import { Router } from '@angular/router';
+
+export interface PlayerInfo {
+  player_id: string;
+  game_id: string;
+}
+export interface StateInfo {
+  Ready: boolean;
+}
 
 @Component({
   selector: 'app-lobby',
@@ -11,18 +20,26 @@ import { GameInfo } from '../services/game-info';
 export class LobbyComponent implements OnInit {
   loading = true;
 
-  gameInfo: GameInfo;
+  playerInfo: PlayerInfo;
+  stateInfo: StateInfo;
 
-  constructor(private webSocketService: WebSocketService) {}
+  constructor(private webSocketService: WebSocketService, private router: Router) {}
 
   ngOnInit(): void {
     this.webSocketService.emit('joinGame', '');
-    this.webSocketService.listen('message').subscribe((data: any) => {
-      this.gameInfo = JSON.parse(data);
-      if (this.gameInfo) {
-        this.loading = false;
-      }
-      console.log(data);
+    this.webSocketService.listen('player_info').subscribe((data: any) => {
+      this.playerInfo = JSON.parse(data);
     });
+    this.webSocketService.listen('state_info').subscribe((data: any) => {
+      this.stateInfo = JSON.parse(data);
+      if (this.stateInfo.Ready) {
+        this.loading = false;
+        this.goToGameInfo();
+      }
+    });
+  }
+
+  goToGameInfo() {
+    this.router.navigate([routes.MULTIPLAYER]);
   }
 }
--- src/app/multiplayer/multiplayer.module.ts
+++ src/app/multiplayer/multiplayer.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialImportsModule } from '../shared/material-imports/material-imports.module';
 import { LobbyComponent } from './lobby/lobby.component';
+import { MultiplayerComponent } from './multiplayer.component';
 import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
-  declarations: [LobbyComponent],
+  declarations: [LobbyComponent, MultiplayerComponent],
   imports: [CommonModule, MaterialImportsModule, AppRoutingModule],
 })
 export class MultiplayerModule {}
