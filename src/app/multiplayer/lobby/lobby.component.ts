c1b8e1f62943d8e8637b8471bb2b3b017448399f
Ole Martin Larsen <ollax@computas.com>
2020-07-21 13:40:14 +0200

    feat(multiplayer): update stateinfo

--- src/app/multiplayer/lobby/lobby.component.ts
+++ src/app/multiplayer/lobby/lobby.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MultiplayerService } from '../services/multiplayer.service';
+import { routes } from '../../shared/models/routes';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lobby',
@@ -7,9 +9,20 @@ import { MultiplayerService } from '../services/multiplayer.service';
   styleUrls: ['./lobby.component.scss'],
 })
 export class LobbyComponent implements OnInit {
-  constructor(public multiPlayerService: MultiplayerService) {}
+  waitingForOtherPlayer = true;
+
+  constructor(public multiPlayerService: MultiplayerService, private router: Router) {}
 
   ngOnInit(): void {
     this.multiPlayerService.joinGame();
+    this.multiPlayerService.stateInfo$.subscribe((obs) => {
+      if (obs.Ready) {
+        this.waitingForOtherPlayer = false;
+      }
+    });
+  }
+
+  goToGameInfo() {
+    this.router.navigate([routes.MULTIPLAYER]);
   }
 }
--- src/app/multiplayer/services/multiplayer.service.ts
+++ src/app/multiplayer/services/multiplayer.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { WebSocketService } from './web-socket.service';
 import { routes } from '../../shared/models/routes';
 import { Router } from '@angular/router';
+import { Observable, Subject, BehaviorSubject, Observer } from 'rxjs';
 
 export interface PlayerInfo {
   player_id: string;
@@ -17,26 +18,27 @@ export class MultiplayerService {
   public loading = true;
 
   playerInfo: PlayerInfo;
-  stateInfo: StateInfo;
+  private readonly _stateInfo = new BehaviorSubject<StateInfo>({ Ready: false });
+
+  readonly stateInfo$ = this._stateInfo.asObservable();
 
   constructor(private webSocketService: WebSocketService, private router: Router) {}
 
   joinGame() {
     this.webSocketService.emit('joinGame', '');
     this.webSocketService.listen('player_info').subscribe((data: any) => {
-      console.log(data);
       this.playerInfo = JSON.parse(data);
     });
     this.webSocketService.listen('state_info').subscribe((data: any) => {
       this.stateInfo = JSON.parse(data);
-      if (this.stateInfo.Ready) {
-        this.loading = false;
-        this.goToGameInfo();
-      }
     });
   }
 
-  private goToGameInfo() {
-    this.router.navigate([routes.MULTIPLAYER]);
+  get stateInfo(): StateInfo {
+    return this._stateInfo.getValue();
+  }
+
+  set stateInfo(val: StateInfo) {
+    this._stateInfo.next(val);
   }
 }
