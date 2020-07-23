a14ee1bc338eee9f7596dc78cd9c0f3cb1ed6d16
Bjørn <bjdax@computas.com>
2020-07-23 10:25:40 +0200

    hardcode height, fix later

--- src/app/route-transition-animations.ts
+++ src/app/route-transition-animations.ts
@@ -8,15 +8,35 @@ export const routeTransitionAnimations = trigger('triggerName', [
                 position: 'absolute',
                 top: 0,
                 right: 0,
-                width: '100%'
+                width: '100%',
+                height: '100%',
             })
         ]),
         query(':enter', [style({ right: '-100%', opacity: 0 })]),
         query(':leave', animateChild()),
         group([
-            query(':leave', [animate('1s ease-out', style({ right: '100%', opacity: 0 }))]),
-            query(':enter', [animate('1s ease-out', style({ right: '0%', opacity: 1 }))])
+            query(':leave', [animate('0.5s ease-out', style({ right: '100%', opacity: 0 }))]),
+            query(':enter', [animate('0.5s ease-out', style({ right: '0%', opacity: 1 }))])
         ]),
         query(':enter', animateChild())
     ]),
+    transition('Three => Two, Two => One, Three => One', [
+        style({ position: 'relative' }),
+        query(':enter, :leave', [
+            style({
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+            })
+        ]),
+        query(':enter', [style({ left: '-100%', opacity: 0 })]),
+        query(':leave', animateChild()),
+        group([
+            query(':leave', [animate('0.5s ease-out', style({ left: '100%', opacity: 0 }))]),
+            query(':enter', [animate('0.5s ease-out', style({ left: '0%', opacity: 1 }))])
+        ]),
+        query(':enter', animateChild())
+    ])
 ]);
