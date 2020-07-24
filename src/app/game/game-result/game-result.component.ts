d86fae0079f70e0fc20b24672d99a976bd35b923
Peder Aalen <pewax@computas.com>
2020-07-24 09:01:25 +0200

    add: check for multiplayer

--- src/app/game/game-result/game-result.component.ts
+++ src/app/game/game-result/game-result.component.ts
@@ -4,6 +4,7 @@ import { Result } from '../../shared/models/result.interface';
 import { Highscore, HighScoreService } from 'src/app/services/highscore.service';
 import { Entry } from 'src/app/services/highscore-entry.interface';
 import { Router } from '@angular/router';
+import { MultiplayerService } from 'src/app/multiplayer/services/multiplayer.service';
 
 @Component({
   selector: 'app-game-result',
@@ -13,18 +14,23 @@ import { Router } from '@angular/router';
 export class GameResultComponent implements OnInit {
   results: Result[] = [];
   dailyHighScores: Highscore[];
+  totalScore: number;
   loading: boolean;
   value = '';
   constructor(
     private drawingService: DrawingService,
-    private highScoreService: HighScoreService,
+    private multiplayerService: MultiplayerService,
     private router: Router
   ) {}
 
   ngOnInit(): void {
-    const totalScore = this.drawingService.results.reduce((accumulator: any, currentValue: any) => {
-      return accumulator.score + currentValue.score;
-    });
+    if (this.multiplayerService.isMultiplayer) {
+      const totalScore = this.drawingService.results.reduce((accumulator: any, currentValue: any) => {
+        return accumulator.score + currentValue.score;
+      });
+      console.log('totalScore', totalScore);
+    }
+
     if (this.router.url === '/summary') {
       this.results = this.drawingService.get();
     } else {
