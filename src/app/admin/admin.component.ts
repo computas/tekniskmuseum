65dca381a16d4d6c6c2f059aeb5a0efb60118d0f
joaaax <joaax@computas.com>
2020-07-27 10:03:50 +0200

    Add admin component

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from './login.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.scss'],
+})
+export class AdminComponent implements OnInit {
+  errorMsg = 'Feil brukernavn eller passord!';
+  constructor(private router: Router, private loginService: LoginService, private _snackBar: MatSnackBar) {}
+
+  ngOnInit(): void {}
+
+  login(username, password) {
+    // Call on login service to authorize user
+    this.loginService.login(username, password).subscribe(
+      (res: any) => {
+        if (res.status) {
+          this.router.navigate(['admin/info']);
+        } else {
+          this.openSnackBar(this.errorMsg);
+        }
+      },
+      (error) => {
+        this.openSnackBar(this.errorMsg);
+      }
+    );
+  }
+
+  openSnackBar(msg = 'suksess!') {
+    this._snackBar.open(msg, 'Lukk', {
+      duration: 4000,
+    });
+  }
+}
