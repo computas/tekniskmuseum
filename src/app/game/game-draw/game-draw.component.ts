ea3c333a6b7238a3821156ae189d702a1187dc65
Bjørn <bjdax@computas.com>
2020-07-27 14:00:24 +0200

    fix linter long line

--- src/app/game/game-draw/game-draw.component.ts
+++ src/app/game/game-draw/game-draw.component.ts
@@ -169,7 +169,11 @@ export class GameDrawComponent implements OnInit, OnDestroy {
         this.drawingService.classify(formData).subscribe((res) => {
           const sortedCertaintyArr = this.sortOnCertainty(res);
           if (sortedCertaintyArr && sortedCertaintyArr.length > 1) {
-            (sortedCertaintyArr[0].label === this.guessWord) ? this.AI_GUESS = sortedCertaintyArr[1].label : this.AI_GUESS = sortedCertaintyArr[0].label;
+            if (sortedCertaintyArr[0].label === this.guessWord) {
+              this.AI_GUESS = sortedCertaintyArr[1].label;
+            } else {
+              this.AI_GUESS = sortedCertaintyArr[0].label;
+            }
           }
           if (res.roundIsDone) {
             this.playResultSound(res.hasWon);
