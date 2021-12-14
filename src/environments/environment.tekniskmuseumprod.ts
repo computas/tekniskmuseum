a6c7bf6f6db4f8cf170d90161cbabbacd472e208
Lars Narve Aune <93524071+lna-computas-com@users.noreply.github.com>
2021-12-14 15:42:02 +0100

    Update environment.tekniskmuseumprod.ts

--- src/environments/environment.tekniskmuseumprod.ts
+++ src/environments/environment.tekniskmuseumprod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   WS_ENDPOINT: 'wss://tekniskback-mp.azurewebsites.net',
-  TEKNISKBACKEND_ENDPOINT :'https://tekniskback.azurewebsites.net',
+  // TEKNISKBACKEND_ENDPOINT :'https://tekniskback.azurewebsites.net',
+  TEKNISKBACKEND_ENDPOINT :'http://localhost:8000',
   inactivityTime: 60 * 1000,
 };
