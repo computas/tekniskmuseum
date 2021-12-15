d11f1da6b124bda57099dcceed87854e60fb5729
computas-hal <hal@computas.com>
2021-12-15 10:06:06 +0100

    Changed wss endpoint to multiplayer backend

--- src/environments/environment.tekniskmuseumprod.ts
+++ src/environments/environment.tekniskmuseumprod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  WS_ENDPOINT: 'wss://tekniskmuseumbackendsingleplayer.azurewebsites.net',
+  WS_ENDPOINT: 'wss://tekniskmuseumbackendmultiplayer.azurewebsites.net',
   // TEKNISKBACKEND_ENDPOINT :'https://tekniskback.azurewebsites.net',
   TEKNISKBACKEND_ENDPOINT :'https://tekniskmuseumbackendsingleplayer.azurewebsites.net',
   inactivityTime: 60 * 1000,
