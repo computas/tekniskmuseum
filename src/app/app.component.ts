8fa66d5b4b8c0f4bf749672bf6690defab0f43dc
Erik Tallang <e.tallang@gmail.com>
2024-06-25 12:15:36 +0200

    chore: format project using prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,12 @@ import { routeTransitionAnimations } from './route-transition-animations';
 import { environment } from '../environments/environment';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    animations: [routeTransitionAnimations],
-    standalone: true,
-    imports: [RouterOutlet],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  animations: [routeTransitionAnimations],
+  standalone: true,
+  imports: [RouterOutlet],
 })
 export class AppComponent implements OnInit {
   title = 'Teknisk Museum';
