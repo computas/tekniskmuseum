a2588cb51930aa4e5250093d8b0b3d876beb8b0c
sondrefj <sonax@computas.com>
2024-07-03 10:52:37 +0200

    refactor: update component to use new service

--- src/app/game/game-intermediate-result/game-progress-bar/game-progress-bar.component.ts
+++ src/app/game/game-intermediate-result/game-progress-bar/game-progress-bar.component.ts
@@ -1,32 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { DrawingService } from '../../game-draw/services/drawing.service';
+import { GameConfig, GameConfigService } from '../../game-config.service';
 import { TranslationService } from '@/app/services/translation.service';
 import { TranslatePipe } from '@/app/pipes/translation.pipe';
 
 @Component({
   selector: 'app-game-progress-bar',
   standalone: true,
-  imports: [
-    TranslatePipe
-  ],
+  imports: [TranslatePipe],
   templateUrl: './game-progress-bar.component.html',
-  styleUrl: './game-progress-bar.component.scss'
+  styleUrl: './game-progress-bar.component.scss',
 })
 export class GameProgressBarComponent implements OnInit {
-  roundNumberProgress: string = ''
+  roundNumberProgress: string = '';
 
-  constructor(private drawingService: DrawingService, private translationService: TranslationService){}
-  
+  constructor(
+    private gameConfigService: GameConfigService,
+    private drawingService: DrawingService,
+    private translationService: TranslationService
+  ) {}
+
   ngOnInit(): void {
-    this.translationService.lang$.subscribe(lang => {
+    this.translationService.lang$.subscribe((lang) => {
       this.translationService.loadTranslations(lang).subscribe();
     });
-    this.roundNumberProgress = this.getRoundProgressText()
+    this.roundNumberProgress = this.getRoundProgressText();
   }
 
   getRoundProgressText(): string {
     const currentRoundNumber = this.drawingService.guessUsed;
-    const numberOfRounds = this.drawingService.totalGuess
-    return `${currentRoundNumber} / ${numberOfRounds}`
+    const numberOfRounds = this.gameConfigService.getConfig.rounds;
+    return `${currentRoundNumber} / ${numberOfRounds}`;
   }
 }
