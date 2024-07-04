af3038caa4d050468d419239912e468f1d63f3a1
Victoria Kallerud <vkalx@computas.com>
2024-07-04 15:51:04 +0200

    style: file with hex values for custom colors

---
+++ src/app/shared/customColors.ts
@@ -0,0 +1,18 @@
+export enum CustomColorsIO {
+    //Text colors
+    white = '#FFFFFF',
+    black = '#000000',
+
+    //Speech bubble colors
+    pastelBlue = '#BCE1E6',
+
+    //Linear gradient
+    cobaltBlue = '#151CDB',
+    indigo = '#5F2BB2',
+    violet = '#B14286',
+    hotPink = '#FD595D',
+    peach = '#E17C6B',
+    gold = '#D2AB72',
+    teal = '#80B19D',
+    turquoise = '#1BC0D2',
+}
