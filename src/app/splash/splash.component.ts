38506bc9584550d3f3d56557639386ad07e49cd1
Jamila Mehmandarova <jammx@computas.com>
2024-07-09 19:06:00 +0200

    add: speeach-bubble, text nad styling added

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { TranslatePipe } from "../core/translation.pipe";
+import { SpeechBubbleComponent } from "../game/speech-bubble/speech-bubble.component";
+import { CustomColorsIO } from '../shared/customColors';
+import { ArrowAlignment, PointerSide } from '../shared/models/interfaces';
 
 @Component({
   selector: 'app-splash',
   templateUrl: './splash.component.html',
   styleUrls: ['./splash.component.scss'],
   standalone: true,
-  imports: [CommonModule, TranslatePipe]
+  imports: [SpeechBubbleComponent, TranslatePipe]
 })
 export class SplashComponent {
-    private headerClicks = 0;
-    
+    textColor: CustomColorsIO = CustomColorsIO.black;
+    PointerSide = PointerSide;
+    ArrowAlignment = ArrowAlignment;
+    CustomColorsIO = CustomColorsIO;
+
     constructor(private router: Router) { }
     
     navigateToWelcome() {
