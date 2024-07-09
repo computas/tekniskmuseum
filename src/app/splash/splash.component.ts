9514a975d7a680de628999fe23ad25efb4b6801e
Jamila Mehmandarova <jammx@computas.com>
2024-07-09 17:53:13 +0200

    fix: text now uses translation pipe

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { TranslatePipe } from "../core/translation.pipe";
 
 @Component({
   selector: 'app-splash',
   templateUrl: './splash.component.html',
   styleUrls: ['./splash.component.scss'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule, TranslatePipe]
 })
 export class SplashComponent {
     private headerClicks = 0;
