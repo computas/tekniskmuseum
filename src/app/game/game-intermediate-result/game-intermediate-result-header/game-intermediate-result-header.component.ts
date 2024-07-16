c01c01bed4f388b242f1a1ae8775f50ee66e532b
sondrefj <sonax@computas.com>
2024-07-16 11:25:04 +0200

    style: update style on button hover and active state

--- src/app/game/game-intermediate-result/game-intermediate-result-header/game-intermediate-result-header.component.ts
+++ src/app/game/game-intermediate-result/game-intermediate-result-header/game-intermediate-result-header.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { routes } from '@/app/shared/models/routes';
 import { TranslatePipe } from '@/app/core/translation.pipe';
 import { GameStateService } from '../../services/game-state-service';
+import { MultiplayerService } from '../../services/multiplayer.service';
 
 @Component({
   selector: 'app-game-intermediate-result-header',
@@ -13,10 +14,19 @@ import { GameStateService } from '../../services/game-state-service';
   styleUrl: './game-intermediate-result-header.component.scss',
 })
 export class GameIntermediateResultHeaderComponent {
-  constructor(private router: Router, private gameStateService: GameStateService) {}
+  constructor(
+    private router: Router,
+    private gameStateService: GameStateService,
+    private multiplayerService: MultiplayerService
+  ) {}
 
-  goToHomePage() {
+  goToWelcomePage() {
     this.gameStateService.clearState();
+
+    if (this.gameStateService.isMultiplayer()) {
+      this.multiplayerService.clearState();
+    }
+
     this.router.navigate([routes.WELCOME]);
   }
 }
