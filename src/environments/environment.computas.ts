da0f2cc0d886e186376bcf2653409935514f4d04
Victoria Kallerud <vkalx@computas.com>
2024-07-17 08:42:15 +0200

    tweak: increased inactivityTime

--- src/environments/environment.computas.ts
+++ src/environments/environment.computas.ts
@@ -3,6 +3,6 @@ export const environment = {
   WS_ENDPOINT: 'wss://teknisk-museum-backend.azurewebsites.net',
   // TEKNISKBACKEND_ENDPOINT :'https://tekniskback.azurewebsites.net',
   TEKNISKBACKEND_ENDPOINT: 'https://teknisk-museum-backend-singleplayer.azurewebsites.net',
-  inactivityTime: 15 * 1000, // 15 seconds
+  inactivityTime: 25 *  1000, // 15 seconds
   PAIR_ID: 'f368b28b-4d39-4362-8c8c-c1f230c64fb0',
 };
