4c647369a6405d68bac461ed246ce31d43cf3563
sondrefj <sonax@computas.com>
2024-07-22 12:03:16 +0200

    style: resize example drawing to fit screen

--- src/app/game/game-intermediate-result/game-example-drawings/wrong-guess/wrong-guess.component.ts
+++ src/app/game/game-intermediate-result/game-example-drawings/wrong-guess/wrong-guess.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SpeechBubbleComponent } from '@/app/game/speech-bubble/speech-bubble.component';
 import { OAvatarComponent } from '@/assets/avatars/o-avatar/o-avatar.component';
 import { ArrowAlignment, PointerSide } from '@/app/shared/models/interfaces';
@@ -12,11 +12,18 @@ import { TranslatePipe } from '@/app/core/translation.pipe';
   templateUrl: './wrong-guess.component.html',
   styleUrl: './wrong-guess.component.scss',
 })
-export class WrongGuessComponent {
+export class WrongGuessComponent implements OnInit {
   PointerSide = PointerSide;
   ArrowAlignment = ArrowAlignment;
   CustomColorsIO = CustomColorsIO;
   label = 'Tiger';
   exampleDrawings: string[] = [];
   guessedDrawings: string[] = [];
+
+  ngOnInit(): void {
+    for (let i = 0; i < 2; i++) {
+      this.exampleDrawings.push('example' + i + '.jpg');
+      this.guessedDrawings.push('guess' + i + '.jpg');
+    }
+  }
 }
