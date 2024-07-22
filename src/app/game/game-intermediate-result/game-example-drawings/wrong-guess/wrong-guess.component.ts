fb572c41671ef970b153b90eb4b3ce521a2ef04e
sondrefj <sonax@computas.com>
2024-07-22 11:26:49 +0200

    style: add basic style and extra speech bubble

--- src/app/game/game-intermediate-result/game-example-drawings/wrong-guess/wrong-guess.component.ts
+++ src/app/game/game-intermediate-result/game-example-drawings/wrong-guess/wrong-guess.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { SpeechBubbleComponent } from '@/app/game/speech-bubble/speech-bubble.component';
+import { OAvatarComponent } from '@/assets/avatars/o-avatar/o-avatar.component';
+import { ArrowAlignment, PointerSide } from '@/app/shared/models/interfaces';
+import { CustomColorsIO } from '@/app/shared/customColors';
+import { TranslatePipe } from '@/app/core/translation.pipe';
 
 @Component({
   selector: 'app-wrong-guess',
   standalone: true,
-  imports: [],
+  imports: [SpeechBubbleComponent, OAvatarComponent, TranslatePipe],
   templateUrl: './wrong-guess.component.html',
-  styleUrl: './wrong-guess.component.scss'
+  styleUrl: './wrong-guess.component.scss',
 })
 export class WrongGuessComponent {
-
+  PointerSide = PointerSide;
+  ArrowAlignment = ArrowAlignment;
+  CustomColorsIO = CustomColorsIO;
+  label = '';
+  exampleDrawings: string[] = [];
+  guessedDrawings: string[] = [];
 }
