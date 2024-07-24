4b05142fd1cf1b0a539eb3933b83cf7e6b0877b6
sondrefj <sonax@computas.com>
2024-07-24 12:59:42 +0200

    refactor: fix translation and score that was hidden

--- src/app/game/game-intermediate-result/game-drawing-display/game-drawing-display.component.ts
+++ src/app/game/game-intermediate-result/game-drawing-display/game-drawing-display.component.ts
@@ -19,6 +19,7 @@ export class GameDrawingDisplayComponent implements OnInit {
 
   ngOnInit(): void {
     this.secondsUsed = this.drawingService.getSecondsUsed();
+
     this.drawingService.resetSecondsUsed();
   }
 
