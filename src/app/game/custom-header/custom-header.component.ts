61389b067ddb4c10d9d5e1c96d94ae8943d4d563
filvanh <fhan@computas.com>
2024-09-17 10:06:13 +0200

    Opprett en felles header component

---
+++ src/app/game/custom-header/custom-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './custom-header.component.html',
+  styleUrl: './custom-header.component.scss'
+})
+export class CustomHeaderComponent {
+
+}
