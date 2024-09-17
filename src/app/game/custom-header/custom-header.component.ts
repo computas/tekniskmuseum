5fff9d028eefb6201246b83ecb696de3353591e3
filvanh <fhan@computas.com>
2024-09-17 17:35:13 +0200

    Endre logikk i customHeader, så den matcher customButton logikk

--- src/app/game/custom-header/custom-header.component.ts
+++ src/app/game/custom-header/custom-header.component.ts
@@ -5,32 +5,20 @@ import { CustomButtonComponent } from '../custom-button/custom-button.component'
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmExitDialogComponent } from '../game-intermediate-result/game-intermediate-result-header/confirm-exit-dialog/confirm-exit-dialog.component';
 import { ButtonStyleClass } from '@/app/shared/buttonStyles';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-custom-header',
   standalone: true,
-  imports: [GameProgressBarComponent, TranslatePipe, CustomButtonComponent, RouterLink, RouterLinkActive], 
+  imports: [GameProgressBarComponent, TranslatePipe, CustomButtonComponent, CommonModule], 
   templateUrl: './custom-header.component.html',
   styleUrl: './custom-header.component.scss'
 })
 export class CustomHeaderComponent {
-  //Custom button input, 
   @Input() buttonStyle: ButtonStyleClass = ButtonStyleClass.default; // Allows customization of the button style
   @Input() buttonText: string = 'Button'; // Default button text
   @Input() iconSrc = ""; //remove this and put this as a constant logic in custom button, since every back button has same logo, and and home button has same logo. 
-  @Input() routerLink?: string;
-  @Input() activeRouter?: boolean; 
-  // Progress bar to be shown
   @Input() showProgressBar: boolean = false;
 
-
-  readonly dialog = inject(MatDialog);
-
-  openDialog(): void {
-    this.dialog.open(ConfirmExitDialogComponent);
-  }
-
-  
 
 }
