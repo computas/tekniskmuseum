6ab9414fdf7213d4466649bedf398d6a30982fcc
Tobias Hermansen <ther@computas.com>
2024-09-26 09:19:19 +0200

    change backend endpoint to containerized version

--- src/environments/environment.computas.ts
+++ src/environments/environment.computas.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   WS_ENDPOINT: 'wss://tekniskmuseumbackendmultiplayer-dev.azurewebsites.net',
   // TEKNISKBACKEND_ENDPOINT :'https://tekniskback.azurewebsites.net',
-  TEKNISKBACKEND_ENDPOINT: 'https://tekniskmuseumbackendsingleplayer-dev.azurewebsites.net',
+  TEKNISKBACKEND_ENDPOINT: 'https://backend-container-cycmafgsckb8b6gu.northeurope-01.azurewebsites.net',
   inactivityTime: 25 * 1000, // 25 seconds
   PAIR_ID: 'f368b28b-4d39-4362-8c8c-c1f230c64fb0',
 };
